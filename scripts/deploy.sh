7bba39a3f9c33a57d100d8ee324edb697e42e7da
ICCHOI <cicblck@gmail.com>
2023-01-28 20:37:08 +0900

    chore : deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,4 +27,7 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar -Dspring.profiles.active=prod,oauth $JAR_NAME 2>&1 &
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-prod.yml \
+  -Dspring.profile.active=prod,oauth \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
