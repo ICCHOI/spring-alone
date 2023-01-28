3c8ab4c8440ea0c2d617a835948b0d7363e3fcf1
ICCHOI <cicblck@gmail.com>
2023-01-28 17:09:15 +0900

    chore : 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -48,7 +48,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-prod.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-prod-db.yml \
-    -Dspring.profiles.active=prod, oauth \
-    $JAR_NAME 2>&1 &
+nohup java -jar -Dspring.profiles.active=prod,oauth $JAR_NAME 2>&1 &
