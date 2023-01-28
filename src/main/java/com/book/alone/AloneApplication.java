6ff90fa01725a4a374a153a74afc9b103e4a4693
ICCHOI <cicblck@gmail.com>
2023-01-28 20:08:49 +0900

    chore : pid 설정 및 script 작성

--- src/main/java/com/book/alone/AloneApplication.java
+++ src/main/java/com/book/alone/AloneApplication.java
@@ -2,12 +2,15 @@ package com.book.alone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AloneApplication.class, args);
+		SpringApplication application = new SpringApplication(AloneApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
